9c2bdc308f1caa7b0ee84e3353f57a1ce7d98ca4
Ghali Beyram <ghalibeyram@ghalis-mac.local>
2018-09-10 02:19:58 -0700

    // add pod file

--- Evrika/GetStartedVC.swift
+++ Evrika/GetStartedVC.swift
@@ -32,15 +32,5 @@ class GetStartedVC: UIViewController {
             typeUser.setTitle("candidate", for: .normal)
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
