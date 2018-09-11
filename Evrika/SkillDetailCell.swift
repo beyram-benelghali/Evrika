087a7438cbcb660a29a78f9fe618858ba533ecde
Ghali Beyram <ghalibeyram@ghalis-mac.local>
2018-09-11 08:34:03 -0700

    // fix

--- Evrika/SkillDetailCell.swift
+++ Evrika/SkillDetailCell.swift
@@ -16,12 +16,6 @@ class SkillDetailCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
     func configureWithItem(item: String) {
         skillName?.text = item
